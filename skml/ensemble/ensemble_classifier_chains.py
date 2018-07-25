6585929933c290cf8ec0b9ca102ec3f88d48a762
asdhob <asd.dhobale@gmail.com>
2018-07-24 21:57:51 -0500

    Update ensemble_classifier_chains.py

--- skml/ensemble/ensemble_classifier_chains.py
+++ skml/ensemble/ensemble_classifier_chains.py
@@ -107,6 +107,7 @@ class EnsembleClassifierChain(
                         return min(x[x<self.threshold])
 
         preds = np.array([cc.predict(X) for cc in self.estimators_])
+        print(preds)
         preds = np.apply_along_axis(func1d=get_best_prob, axis=0,arr=preds)
 #        preds = np.mean(preds, axis=0)
 #         W_norm = preds.mean(axis=0)
